d60b9b26e1b4dbf32bc04217103b992cdd66d918
Anna <57022302+AnnaToussaint@users.noreply.github.com>
2021-12-25 16:46:23 -0400

    Delete RNA_secondary_structure_prediction.jl

--- RNA_secondary_structure_prediction.jl
+++
@@ -1,8 +0,0 @@
-#using ViennaRNA to predict secondary structures
-
-using ViennaRNA
-
-#selecting uniq_ML=1 is required for recursive functions e.g. pbacktrack
-seq_folded = FoldCompound("ATGATGATGATGATGATGATGATGATGATGATG" ; uniq_ML=1)
-
-print(seq_folded)
